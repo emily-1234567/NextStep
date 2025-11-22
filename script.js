dcef1c48dfa3fe3eddb163f09c6e91a6256b863f
emily-1234567 <emilysmile324@gmail.com>
2025-11-22 12:19:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+const navLinkEls = document.querySelectorAll('.navbar-item');
+const windowPathname = window.location.pathname;
 
+navLinkEls.forEach(navLinkEl => {
+    if (navLinkEl.href.includes(windowPathname))  {
+        navLinkEl.classList.add('active');
+    }
+});
