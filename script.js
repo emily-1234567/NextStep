646f6527aa0586fce7b40b24abf052c1408cb9ad
emily-1234567 <emilysmile324@gmail.com>
2025-12-14 11:29:35 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const navLinkEls = document.querySelectorAll('.navbar-item');
-const windowPathname = window.location.pathname;
-
-navLinkEls.forEach(navLinkEl => {
-    if (navLinkEl.href.includes(windowPathname))  {
-        navLinkEl.classList.add('active');
-    }
-});
